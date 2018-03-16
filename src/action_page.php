5a64dab7ae11f4fb99c0c78e514fa651f170957c
rajula96reddy <rajula96reddy@gmail.com>
2018-03-16 14:50:38 +0530

    change action_page.php

--- src/action_page.php
+++ src/action_page.php
@@ -19,7 +19,7 @@ else
 <!DOCTYPE html>
 <html>
 <head>
-<title>online stats</title>
+<title>online </title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
